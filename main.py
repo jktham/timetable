2b78b6259508531ae236146fcd645e3ae525ed51
jktham-sf <jonas.tham@gmx.ch>
2021-11-10 14:10:16 +0100

    Check if auth successful

--- main.py
+++ main.py
@@ -2,31 +2,44 @@ import json
 import requests
 from datetime import datetime, timedelta
 
+def get_timetable(username, password, userid):
+    with requests.Session() as s:
+        info_response = s.get("https://intranet.tam.ch/krm/timetable/classbook")
+
+        hash = info_response.text.split("name=\"hash\" value=\"", 1)[1].split("\"", 1)[0]
+        session = info_response.headers["Set-Cookie"].split("sturmsession=", 1)[1].split(";", 1)[0]
+
+        payload = {"hash": hash, "loginschool": "krm", "loginuser": username, "loginpassword": password}
+        auth_response = s.post("https://intranet.tam.ch/krm/timetable/classbook", data=payload)
+        if "userId" in auth_response.text:
+            print("got userid: " + auth_response.text.split("userId = \"", 1)[1].split("\"", 1)[0])
+            print("def userid: " + userid)
+        else:
+            print("Authentication failed")
+            return
+            
+        today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        monday = today - timedelta(days=today.weekday())
+        sunday = today + timedelta(days=6) - timedelta(days=today.weekday())
+
+        payload = {"startDate": monday.timestamp()*1000, "endDate": sunday.timestamp()*1000, "studentId[]": userid, "holidaysOnly": 0}
+        cookies = {"username": username, "sturmuser": username, "school": "krm", "sturmsession": session}
+        headers = {"x-requested-with": "XMLHttpRequest"}
+        data_response = s.post("https://intranet.tam.ch/krm/timetable/ajax-get-timetable", data=payload, cookies=cookies, headers=headers)
+        data = data_response.json()["data"]
+    return data
+
 with open("credentials.json") as file:
     cred = json.load(file)
     username = cred["username"]
     password = cred["password"]
     userid = cred["userid"]
 
-with requests.Session() as s:
-    info_response = s.get("https://intranet.tam.ch/krm/timetable/classbook")
+data = get_timetable(username, password, userid)
 
-    hash = info_response.text.split("name=\"hash\" value=\"", 1)[1].split("\"", 1)[0]
-    session = info_response.headers["Set-Cookie"].split("sturmsession=", 1)[1].split(";", 1)[0]
-
-    payload = {"hash": hash, "loginschool": "krm", "loginuser": username, "loginpassword": password}
-    auth_response = s.post("https://intranet.tam.ch/krm/timetable/classbook", data=payload)
-
-    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    monday = today - timedelta(days=today.weekday())
-    sunday = today + timedelta(days=6) - timedelta(days=today.weekday())
-
-    payload = {"startDate": monday.timestamp()*1000, "endDate": sunday.timestamp()*1000, "studentId[]": userid, "holidaysOnly": 0}
-    cookies = {"username": username, "sturmuser": username, "school": "krm", "sturmsession": session}
-    headers = {"x-requested-with": "XMLHttpRequest"}
-    data_response = s.post("https://intranet.tam.ch/krm/timetable/ajax-get-timetable", data=payload, cookies=cookies, headers=headers)
-    data = data_response.json()["data"]
-
-
-for i in range(len(data)):
-    print(data[i]["lessonDate"], data[i]["lessonStart"], data[i]["courseName"], data[i]["roomName"])
+if data:
+    for i in range(len(data)):
+        print(data[i]["lessonDate"], data[i]["lessonStart"], data[i]["courseName"], data[i]["roomName"])
+    print(len(data))
+else:
+    print("No data")
